94228f52be0fa7deae70dd399985a0502962b4ca
Joshuabrotman <joshuabrotman@Gmail.com>
2020-04-06 20:26:33 -0500

    first commit

---
+++ Sweepstakes/Sweepstakes/Contestant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        public string lName;
+        public string fName;
+        public string eMail;
+        public Dictionary<string, int> registrationNum;
+
+        public Contestant()
+        {
+
+        }
+
+    }
+}
---
+++ Sweepstakes/Sweepstakes/UserInterface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class UserInterface
+    {
+
+        public void AddContestant()
+        {
+            Contestant NewConstestant = new Contestant();
+            Console.WriteLine("Please enter the contestants first name:");
+            NewConstestant.fName = Console.ReadLine();
+            Console.WriteLine("Please enter the contestants last name:");
+            NewConstestant.lName = Console.ReadLine();
+            Console.WriteLine("Please enter the contestants E-Mail:");
+            NewConstestant.eMail = Console.ReadLine();
+
+        }
+    }
+}
